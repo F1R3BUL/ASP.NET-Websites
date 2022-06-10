765e0b4763cd5adbd8f2595035eb5e3c7c141607
F1R3BUL <53118483+F1R3BUL@users.noreply.github.com>
2022-06-10 10:31:33 +0300

    .

---
+++ Account_Manager/Account_Manager/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Account_Manager.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Password { get; set; }
+    }
+}
